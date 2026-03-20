866b1625b5516a75c5608ba50f3e66ff1aa68c4b
Vinicius Stoc <vmartinsstoc@gmail.com>
2026-03-20 14:41:29 -0300

    UPLOAD/ Alteração envio email outlook para gmail

--- config/settings.py
+++ config/settings.py
@@ -155,8 +155,8 @@ LOGOUT_REDIRECT_URL = 'login'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'backupti@i9tmg.com.br'
+EMAIL_HOST_USER = 'inovetmgti@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
